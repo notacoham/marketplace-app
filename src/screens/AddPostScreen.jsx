7e90caf79327f08381cee940ceb1ef33742fa308
notacoham <alexcottam12@gmail.com>
2025-07-11 22:13:31 -0600

    add basic form and styling

--- src/screens/AddPostScreen.jsx
+++ src/screens/AddPostScreen.jsx
@@ -1,7 +1,16 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { app } from "../../firebaseConfig";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { Formik } from "formik";
+import { Picker } from "@react-native-picker/picker";
 
 export default function AddPostScreen() {
   const db = getFirestore(app);
@@ -12,6 +21,7 @@ export default function AddPostScreen() {
   }, []);
 
   const getCategoryList = async () => {
+    setCategoryList([]);
     const querySnapshot = await getDocs(collection(db, "Category"));
 
     querySnapshot.forEach((doc) => {
@@ -20,8 +30,102 @@ export default function AddPostScreen() {
     });
   };
   return (
-    <View>
-      <Text>AddPostScreen</Text>
+    <View className="p-10">
+      <Text className="text-[27px] font-bold">Add New Post</Text>
+      <Text className="text-[18px] text-gray-500 mb-7">
+        Create New Post and Start Selling
+      </Text>
+      <Formik
+        initialValues={{
+          name: "",
+          desc: "",
+          category: "",
+          address: "",
+          price: "",
+        }}
+        onSubmit={(value) => console.log(value)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          setFieldValue,
+        }) => {
+          return (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Title"
+                value={values?.title}
+                onChangeText={handleChange("title")}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Description"
+                value={values?.desc}
+                numberOfLines={5}
+                onChangeText={handleChange("desc")}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Price"
+                value={values?.price}
+                keyboardType="numeric"
+                onChangeText={handleChange("price")}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Address"
+                value={values?.address}
+                onChangeText={handleChange("address")}
+              />
+              {/* Category List Dropdown */}
+              <View style={{ borderWidth: 1, borderRadius: 10, marginTop: 15 }}>
+                <Picker
+                  className="border-2"
+                  selectedValue={values?.category}
+                  onValueChange={(itemValue) =>
+                    setFieldValue("category", itemValue)
+                  }
+                >
+                  {categoryList &&
+                    categoryList.map((item, index) => {
+                      return (
+                        <Picker.Item
+                          key={index}
+                          label={item?.name}
+                          value={item?.name}
+                        />
+                      );
+                    })}
+                </Picker>
+              </View>
+              <TouchableOpacity
+                onPress={handleSubmit}
+                className="p-4 bg-blue-500 rounded-full mt-10"
+              >
+                <Text className="text-white text-center text-[16px] ">
+                  Submit
+                </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      </Formik>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 15,
+    paddingHorizontal: 17,
+    fontSize: 17,
+    marginTop: 10,
+    marginBottom: 5,
+    textAlignVertical: "top",
+  },
+});
